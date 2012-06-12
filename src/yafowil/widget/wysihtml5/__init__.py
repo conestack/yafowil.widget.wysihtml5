4d00562c34aad377cb180a35988a685e78c94104
Johannes Raggam <raggam-nl@adm.at>
2012-06-12 14:57:26 +0200

    fix init

--- src/yafowil/widget/wysihtml5/__init__.py
+++ src/yafowil/widget/wysihtml5/__init__.py
@@ -10,18 +10,26 @@ def get_resource_dir():
 
 
 def get_js():
-    return [{
-        'resource': 'wysihtml5/dist/wysihtml5-0.3.0.min.js',
-        'thirdparty': True,
-        'order': 20,
-    }, {
+    return [
+    {
         'resource': 'wysihtml5/parser_rules/advanced.js',
         'thirdparty': True,
         'order': 20,
-    }, {
+    },
+    {
+        'resource': 'wysihtml5/dist/wysihtml5-0.3.0.js',
+        'thirdparty': True,
+        'order': 21,
+    },
+    #{
+    #    'resource': 'bootstrap-wysihtml5/lib/js/wysihtml5-0.3.0_rc3.js',
+    #    'thirdparty': True,
+    #    'order': 20,
+    #},
+    {
         'resource': 'widget.js',
         'thirdparty': False,
-        'order': 21,
+        'order': 22,
     }]
 
 
--- src/yafowil/widget/wysihtml5/resources/widget.js
+++ src/yafowil/widget/wysihtml5/resources/widget.js
@@ -26,23 +26,31 @@ if (typeof(window['yafowil']) == "undefined") yafowil = {};
             
             binder: function(context) {
 
-                var editor = new wysihtml5.Editor("textarea.wysihtml5", {
-                    parserRules:  wysihtml5ParserRules,
-                    toolbar:      null,
-                    autoLink:     true,
-                    parser:       wysihtml5.dom.parse || Prototype.K,
-                    composerClassName: "wysihtml5-editor",
-                    bodyClassName:     "wysihtml5-supported",
-                    allowObjectResizing:  true,
-                    supportTouchDevices:  true
-                });
-                var wysihtml5_resize_iframe = function() {
-                    editor.composer.iframe.style.height = editor.composer.element.scrollHeight + "px";
-                };
-                editor.observe("load", function () {
-                    editor.composer.element.addEventListener("keyup", wysihtml5_resize_iframe, false);
-                    editor.composer.element.addEventListener("blur", wysihtml5_resize_iframe, false);
-                    editor.composer.element.addEventListener("focus", wysihtml5_resize_iframe, false);
+                $('textarea.wysihtml5', context).each(function(event) {
+                    var id = $(this).attr('id');
+
+                    var editor = new wysihtml5.Editor(id, {
+                        parserRules:  wysihtml5ParserRules,
+                        toolbar:      null
+                        //name:         'yafowil_wysihtml5_editor',
+                        //style:        false,
+                        //autoLink:     true,
+                        //parser:       wysihtml5.dom.parse || Prototype.K,
+                        //composerClassName: "wysihtml5-editor",
+                        //bodyClassName:     "wysihtml5-supported",
+                        //allowObjectResizing:  true,
+                        //supportTouchDevices:  true
+                    });
+
+                    //var wysihtml5_resize_iframe = function() {
+                    //    editor.composer.iframe.style.height = editor.composer.element.scrollHeight + "px";
+                    //};
+                    //editor.observe("load", function () {
+                    //    editor.composer.element.addEventListener("keyup", wysihtml5_resize_iframe, false);
+                    //    editor.composer.element.addEventListener("blur", wysihtml5_resize_iframe, false);
+                    //    editor.composer.element.addEventListener("focus", wysihtml5_resize_iframe, false);
+                    //});
+
                 });
 
             }
