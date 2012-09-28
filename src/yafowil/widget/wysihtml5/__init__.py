4f859630cbb3c3e5883895e46d9cb6286fa76548
Robert Niederreiter <office@squarewave.at>
2012-09-28 10:47:37 +0200

    blanklines

--- src/yafowil/widget/wysihtml5/__init__.py
+++ src/yafowil/widget/wysihtml5/__init__.py
@@ -2,9 +2,7 @@ import os
 from yafowil.base import factory
 
 
-# XXX: use fanstatic
 resourcedir = os.path.join(os.path.dirname(__file__), 'resources')
-
 js = [{
     'group': 'wysihtml',
     'resource': 'wysihtml5/dist/wysihtml5-0.3.0.js',
@@ -22,7 +20,6 @@ js = [{
     'resource': 'widget.js',
     'order': 23,
 }]
-
 css = [{
     'group': 'wysihtml',
     'resource': 'bootstrap-wysihtml5/src/bootstrap-wysihtml5.css',
