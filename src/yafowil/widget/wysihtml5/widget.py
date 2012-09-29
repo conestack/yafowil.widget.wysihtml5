4e44bdf1a5e071a42df7505977cc888dd5c97027
Robert Niederreiter <office@squarewave.at>
2012-09-29 11:20:15 +0200

    blueprint documentation

--- src/yafowil/widget/wysihtml5/widget.py
+++ src/yafowil/widget/wysihtml5/widget.py
@@ -28,8 +28,25 @@ factory.doc['blueprint']['wysihtml5'] = \
 """
 
 factory.defaults['wysihtml5.default'] = ''
+
 factory.defaults['wysihtml5.wrap'] = None
+factory.doc['props']['wysihtml5.wrap'] = \
+"""Either ``soft``, ``hard``, ``virtual``, ``physical`` or  ``off``.
+"""
+
 factory.defaults['wysihtml5.cols'] = 80
+factory.doc['props']['wysihtml5.cols'] = \
+"""Number of characters.
+"""
+
 factory.defaults['wysihtml5.rows'] = 10
+factory.doc['props']['wysihtml5.rows'] = \
+"""Number of lines.
+"""
+
 factory.defaults['wysihtml5.readonly'] = None
+factory.doc['props']['wysihtml5.readonly'] = \
+"""Flag for readonly.
+"""
+
 factory.defaults['wysihtml5.class'] = 'wysihtml5'
