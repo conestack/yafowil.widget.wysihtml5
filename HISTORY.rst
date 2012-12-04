b06d2aa40a2ee6f9ff91b998c2f5d880459101e9
Johannes Raggam <raggam-nl@adm.at>
2012-12-04 02:49:41 +0100

    - Don't register the bootstrap.min.js file. This should be done wether with   yafowil.bootstrap or another Twitter Bootstrap integration package.   [thet]
    - Fix resource group names to reflect the package name.
      [thet]

--- HISTORY.rst
+++ HISTORY.rst
@@ -2,6 +2,17 @@
 History
 =======
 
+1.1 (unreleased)
+----------------
+
+- Don't register the bootstrap.min.js file. This should be done wether with
+  yafowil.bootstrap or another Twitter Bootstrap integration package.
+  [thet]
+
+- Fix resource group names to reflect the package name.
+  [thet]
+
+
 1.0
 ---
 
--- src/yafowil/widget/wysihtml5/__init__.py
+++ src/yafowil/widget/wysihtml5/__init__.py
@@ -4,28 +4,24 @@ from yafowil.base import factory
 
 resourcedir = os.path.join(os.path.dirname(__file__), 'resources')
 js = [{
-    'group': 'yafowil.widget.wysihtml.dependencies',
+    'group': 'yafowil.widget.wysihtml5.dependencies',
     'resource': 'wysihtml5/dist/wysihtml5-0.3.0.js',
     'order': 21,
 }, {
-    'group': 'yafowil.widget.wysihtml.dependencies',
-    'resource': 'bootstrap-wysihtml5/lib/js/bootstrap.min.js',
-    'order': 22,
-}, {
-    'group': 'yafowil.widget.wysihtml.dependencies',
+    'group': 'yafowil.widget.wysihtml5.dependencies',
     'resource': 'bootstrap-wysihtml5/src/bootstrap-wysihtml5.js',
     'order': 22,
 }, {
-    'group': 'yafowil.widget.wysihtml.common',
+    'group': 'yafowil.widget.wysihtml5.common',
     'resource': 'widget.js',
     'order': 23,
 }]
 css = [{
-    'group': 'yafowil.widget.wysihtml.dependencies',
+    'group': 'yafowil.widget.wysihtml5.dependencies',
     'resource': 'bootstrap-wysihtml5/src/bootstrap-wysihtml5.css',
     'order': 21,
 }, {
-    'group': 'yafowil.widget.wysihtml.common',
+    'group': 'yafowil.widget.wysihtml5.common',
     'resource': 'widget.css',
     'order': 22,
 }]
