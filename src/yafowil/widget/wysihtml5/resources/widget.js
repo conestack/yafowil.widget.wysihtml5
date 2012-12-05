3b5af855c7c90bece366675c6a3a3f3c472a0cf2
Johannes Raggam <raggam-nl@adm.at>
2012-12-05 12:28:39 +0100

    use dot-notation to satisfy jslint

--- src/yafowil/widget/wysihtml5/resources/widget.js
+++ src/yafowil/widget/wysihtml5/resources/widget.js
@@ -13,7 +13,7 @@ if (typeof(window['yafowil']) == "undefined") yafowil = {};
         yafowil.wysihtml5.binder();
 
         // add after ajax binding if bdajax present
-        if (typeof(window['bdajax']) != "undefined") {
+        if (typeof(window.bdajax) != "undefined") {
             $.extend(bdajax.binders, {
                 wysihtml5_binder: yafowil.wysihtml5.binder
             });
