a959e27af15e859bbfee17a7ac702d891aa83bf6
Robert Niederreiter <office@squarewave.at>
2014-07-03 15:27:30 +0200

    basic refactoring

--- src/yafowil/widget/wysihtml5/__init__.py
+++ src/yafowil/widget/wysihtml5/__init__.py
@@ -5,11 +5,11 @@ from yafowil.base import factory
 resourcedir = os.path.join(os.path.dirname(__file__), 'resources')
 js = [{
     'group': 'yafowil.widget.wysihtml5.dependencies',
-    'resource': 'wysihtml5/dist/wysihtml5-0.3.0.js',
+    'resource': 'bootstrap3-wysihtml5/lib/js/wysihtml5-0.3.0.js',
     'order': 21,
 }, {
     'group': 'yafowil.widget.wysihtml5.dependencies',
-    'resource': 'bootstrap-wysihtml5/src/bootstrap-wysihtml5.js',
+    'resource': 'bootstrap3-wysihtml5/src/bootstrap3-wysihtml5.js',
     'order': 22,
 }, {
     'group': 'yafowil.widget.wysihtml5.common',
@@ -18,16 +18,12 @@ js = [{
 }]
 css = [{
     'group': 'yafowil.widget.wysihtml5.dependencies',
-    'resource': 'bootstrap-wysihtml5/src/bootstrap-wysihtml5.css',
+    'resource': 'bootstrap3-wysihtml5/src/bootstrap-wysihtml5.css',
     'order': 21,
-}, {
-    'group': 'yafowil.widget.wysihtml5.dependencies',
-    'resource': 'bootstrap-wysihtml5/lib/css/wysiwyg-color.css',
-    'order': 22,
 }, {
     'group': 'yafowil.widget.wysihtml5.common',
     'resource': 'widget.css',
-    'order': 23,
+    'order': 22,
 }]
 
 
--- src/yafowil/widget/wysihtml5/resources/widget.js
+++ src/yafowil/widget/wysihtml5/resources/widget.js
@@ -27,87 +27,8 @@ if (typeof(window.yafowil) == "undefined") yafowil = {};
             binder: function(context) {
 
                 $('textarea.wysihtml5', context).each(function(event) {
-
-                    var extra_keys = ['focus', 'resize', 'stylesheets'];
                     var elem = $(this);
-                    var options = elem.data();
-
-                    function make_options_extra(options, extra_keys) {
-                        // cleanup api options object and move out extra options
-                        var options_extra = {};
-                        for (i=0;i<extra_keys.length;i++) {
-                            key = extra_keys[i];
-                            options_extra[key] = options[key];
-                            delete options[key];
-                        }
-                        return options_extra;
-                    }
-                    options_extra = make_options_extra(options, extra_keys);
-
-                    // options processing
-                    $.fn.wysihtml5.defaultOptions.stylesheets = []; // reset stylesheets in defaultOptions to overwrite the default one.
-                    options.stylesheets = [];
-                    if (options_extra.stylesheets !== undefined) {
-                        if (typeof options_extra.stylesheets === 'string') {
-                            // In case of string
-                            options.stylesheets.push(options_extra.stylesheets);
-                        } else {
-                            // Else case must be a list, but we're not
-                            // explicitly checking it.
-                            options.stylesheets = options_extra.stylesheets;
-                        }
-                    }
-                    options.stylesheets.push('/++resource++yafowil.widget.wysihtml5/widget.css');
-                    if (options.color===true) {
-                        options.stylesheets.push('/++resource++yafowil.widget.wysihtml5/bootstrap-wysihtml5/lib/css/wysiwyg-color.css');
-                    }
-
-                    if (options.justify === true) {
-
-                        $.fn.wysihtml5.defaultOptions.justify = true; // extend defaultOptions
-
-                        options.customTemplates = {
-                            justify: function(locale, options) {
-                                var size = (options && options.size) ? ' btn-'+options.size : '';
-                                return "<li>" +
-                                  "<div class='btn-group'>" +
-                                      "<a class='btn" + size + "' data-wysihtml5-command='justifyLeft' title='Align left'><i class='icon-align-left'></i></a>" +
-                                      "<a class='btn" + size + "' data-wysihtml5-command='justifyCenter' title='Align center'><i class='icon-align-center'></i></a>" +
-                                      "<a class='btn" + size + "' data-wysihtml5-command='justifyRight' title='Align right'><i class='icon-align-right'></i></a>" +
-                                  "</div>" +
-                                "</li>";
-                            }
-                        };
-
-                        options.parserRules = {
-                            classes: {
-                                "wysiwyg-text-align-left": 1,
-                                "wysiwyg-text-align-center": 1,
-                                "wysiwyg-text-align-right": 1
-                            }
-                        };
-
-                    }
-
-                    // create the editor
-                    var editor = elem.wysihtml5('deepExtend', options).data("wysihtml5").editor;
-
-                    // use some extra features
-                    if (options_extra.focus === true) {
-                        editor.on("load", function() {
-                            editor.focus();
-                        });
-                    }
-                    if (options_extra.resize === true) {
-                        var wysihtml5_resize_iframe = function() {
-                            editor.composer.iframe.style.height = editor.composer.element.scrollHeight + "px";
-                        };
-                        editor.observe("load", function () {
-                            editor.composer.element.addEventListener("keyup", wysihtml5_resize_iframe, false);
-                            editor.composer.element.addEventListener("blur", wysihtml5_resize_iframe, false);
-                            editor.composer.element.addEventListener("focus", wysihtml5_resize_iframe, false);
-                        });
-                    }
+                    elem.wysihtml5(); // options are taken from data attributes
                 });
 
             }
