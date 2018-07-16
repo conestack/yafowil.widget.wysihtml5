f759c5e6732f0321c94ff2b29ca67985265d1f69
Robert Niederreiter <office@squarewave.at>
2018-07-16 16:21:06 +0200

    prepare 1.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 import os
 
 
-version = '1.4.dev0'
+version = '1.4'
 shortdesc = 'wysihtml5 richtext widget for YAFOWIL with bootstrap integration'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'HISTORY.rst')).read()
